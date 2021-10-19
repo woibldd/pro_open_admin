1cb9499cbf4cabc541cdee573a88692012d5f632
caoshiyuan <codesircao@126.com>
2021-10-19 14:31:04 +0800

    add: vue page

---
+++ vue-admin-template/src/store/modules/user.js
@@ -0,0 +1,102 @@
+import { login, logout, getInfo } from "@/api/user";
+import { getToken, setToken, removeToken } from "@/utils/auth";
+import { resetRouter } from "@/router";
+
+const getDefaultState = () => {
+  return {
+    token: getToken(),
+    name: "",
+    avatar: "",
+    userInfo: {
+      name: ""
+    }
+  };
+};
+
+const state = getDefaultState();
+
+const mutations = {
+  RESET_STATE: state => {
+    Object.assign(state, getDefaultState());
+  },
+  SET_TOKEN: (state, token) => {
+    state.token = token;
+  },
+  SET_USER_INFO: (state, data) => {
+    Object.assign(state.userInfo, data);
+  }
+};
+
+const actions = {
+  // user login
+  login({ commit }, userInfo) {
+    const { username, password } = userInfo;
+    return new Promise((resolve, reject) => {
+      login({ loginName: username.trim(), password: password })
+        .then(response => {
+          const { data } = response;
+          commit("SET_TOKEN", data.jwtToken);
+          commit("SET_USER_INFO", { username });
+          setToken(data.jwtToken);
+          resolve();
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  // get user info
+  getInfo({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      getInfo(state.token)
+        .then(response => {
+          const { data } = response;
+          if (!data) {
+            return reject("Verification failed, please Login again.");
+          }
+          commit("SET_USER_INFO", data);
+          resolve(data);
+        })
+        .catch(error => {
+          reject(error);
+        });
+    });
+  },
+
+  // user logout
+  logout({ commit, state }) {
+    return new Promise((resolve, reject) => {
+      removeToken();
+      resetRouter();
+      commit("RESET_STATE");
+      resolve();
+      // logout(state.token)
+      //   .then(() => {
+      //     removeToken(); // must remove  token  first
+      //     resetRouter();
+      //     commit("RESET_STATE");
+      //     resolve();
+      //   })
+      //   .catch(error => {
+      //     reject(error);
+      //   });
+    });
+  },
+
+  // remove token
+  resetToken({ commit }) {
+    return new Promise(resolve => {
+      removeToken(); // must remove  token  first
+      commit("RESET_STATE");
+      resolve();
+    });
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+};

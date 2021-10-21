3a48c5c09d660dd8cbe0a86e4a6e0b90d45e4ad2
caoshiyuan <codesircao@126.com>
2021-10-21 17:22:19 +0800

    add: 权限

---
+++ vue-admin-template/src/views/userManagemen/list.vue
@@ -0,0 +1,269 @@
+<template>
+  <div class="app-container">
+    <ContainerHeader :title="$route.meta.title" />
+    <div class="filter-container">
+      <el-form :inline="true">
+        <el-form-item label="关键字搜索">
+          <el-input v-model="listQuery.title" placeholder="用户名称" style="width: 200px;" class="filter-item" @keyup.enter.native="handleFilter" />
+        </el-form-item>
+        <!-- <el-form-item label="用户状态">
+          <el-select v-model="listQuery.type" placeholder="审核状态" clearable class="filter-item" style="width: 130px">
+            <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name + '(' + item.key + ')'" :value="item.key" />
+          </el-select>
+        </el-form-item> -->
+
+        <!-- <el-select v-model="listQuery.sort" style="width: 140px" class="filter-item" @change="handleFilter">
+        <el-option v-for="item in sortOptions" :key="item.key" :label="item.label" :value="item.key" />
+      </el-select> -->
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+          搜索
+        </el-button>
+        <el-button v-waves class="filter-item" type="primary" icon="el-icon-edit" @click="handleUser('add')">
+          新增
+        </el-button>
+      </el-form>
+    </div>
+
+    <el-table :key="tableKey" v-loading="listLoading" :data="list" border fit highlight-current-row style="width: 100%;" @row-click="handleRowClick">
+      <el-table-column v-for="col in TableColumus" :key="col.id" :label="col.label" :width="col.width || 'auto'" align="center">
+        <template slot-scope="{ row }">
+          <span>{{ row[col.key] }}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" fixed="right" align="center" width="230" class-name="small-padding fixed-width">
+        <template slot-scope="{ row }">
+          <el-button type="danger" size="mini" @click.stop="handleUser('edit', row)">
+            修改
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination v-show="total > 0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.pageSize" @pagination="getList" />
+
+    <el-dialog :title="dialogType === 'edit' ? '编辑用户' : '添加用户'" :visible.sync="dialogFormVisible">
+      <el-form :model="userData" ref="dataForm" label-position="right" label-width="80px">
+        <el-form-item label="用户名" prop="loginName" :rules="[{ required: dialogType == 'add', message: '请填写用户名' }]">
+          <el-input :disabled="dialogType == 'edit'" v-model="userData.loginName" placeholder="请填写用户名" />
+        </el-form-item>
+        <el-form-item label="密码" prop="password" :rules="[{ required: dialogType == 'add', message: '请填写密码' }]">
+          <el-input :disabled="dialogType == 'edit'" v-model="userData.password" type="password" placeholder="请填写修改密码" />
+        </el-form-item>
+        <!-- <el-form-item label="用户描述">
+          <el-input v-model="userData.description" :autosize="{ minRows: 2, maxRows: 4 }" type="textarea" placeholder="用户描述" />
+        </el-form-item> -->
+
+        <el-divider class="title">用户权限</el-divider>
+        <!-- <el-checkbox v-model="checked">全选</el-checkbox> -->
+        <el-tree ref="tree" :check-strictly="checkStrictly" :data="routesData" :props="defaultProps" show-checkbox node-key="permission" class="permission-tree" />
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">
+          取消
+        </el-button>
+        <el-button type="primary" @click="handleSave" :loading="submitLoading">
+          保存
+        </el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { getList, update, add } from '@/api/user'
+import waves from '@/directive/waves' // waves directive
+import { parseTime } from '@/utils'
+import Pagination from '@/components/Pagination' // secondary package based on el-pagination
+import ContainerHeader from '@/components/ContainerHeader'
+import clip from '@/utils/clipboard'
+import { generatePermission } from '@/permission'
+
+const calendarTypeOptions = [
+  { key: 0, display_name: '审核中', tagtype: 'warn' },
+  { key: 1, display_name: '审核通过', tagtype: 'success' },
+  { key: 2, display_name: '审核失败', tagtype: 'danger' }
+]
+const TableColumus = [
+  { label: 'id', key: 'id', type: 'string', show: false, value: '', width: '80px' },
+  { label: '用户名称', key: 'loginName', type: 'string', show: false, value: '' },
+  { label: '更新时间', key: 'update_time', type: 'string', show: false, value: '', filter: 'parseTime' }
+]
+const initUserData = {
+  id: '',
+  loginName: '',
+  password: '',
+  permission:[]
+}
+
+export default {
+  name: 'userList',
+  components: { Pagination, ContainerHeader },
+  directives: { waves },
+  filters: {
+    statusFilter(status) {
+      const statusMap = {
+        published: 'success',
+        draft: 'info',
+        deleted: 'danger'
+      }
+      return statusMap[status]
+    },
+    appovalFilter(key) {
+      const { display_name } = calendarTypeOptions.find(v => v.key == key) || {
+        display_name: ''
+      }
+      return display_name
+    },
+    appovalFilterColor(key) {
+      const { tagtype } = calendarTypeOptions.find(v => v.key == key) || {
+        tagtype: 'info'
+      }
+      return tagtype
+    }
+  },
+  computed: {
+    routesData() {
+      const routes = this.$router.options.routes
+      return generatePermission(routes)
+    }
+  },
+  data() {
+    return {
+      TableColumus,
+      submitLoading: false,
+      tableKey: 0,
+      list: [],
+      total: 0,
+      listLoading: true,
+      listQuery: {
+        pageNum: 1,
+        pageSize: 20,
+        importance: undefined,
+        title: undefined,
+        type: undefined,
+        sort: '+id'
+      },
+      calendarTypeOptions,
+      sortOptions: [
+        { label: 'ID Ascending', key: '+id' },
+        { label: 'ID Descending', key: '-id' }
+      ],
+
+      dialogFormVisible: false,
+      dialogType: 'edit',
+      checkStrictly: false,
+      defaultProps: {
+        children: 'children',
+        label: 'title'
+      },
+      userData: JSON.parse(JSON.stringify(initUserData)),
+      rules: {
+        type: [{ required: true, message: 'type is required', trigger: 'change' }],
+        timestamp: [
+          {
+            type: 'date',
+            required: true,
+            message: 'timestamp is required',
+            trigger: 'change'
+          }
+        ],
+        title: [{ required: true, message: 'title is required', trigger: 'blur' }]
+      },
+      downloadLoading: false
+    }
+  },
+
+  created() {
+    this.getList()
+  },
+  methods: {
+    getList(noLoading) {
+      if (!noLoading) {
+        this.listLoading = true
+      }
+      getList(this.listQuery).then(response => {
+        this.list = response.data.list.map(v => {
+          v.update_time = parseTime(v.update_time)
+          return v
+        })
+        this.total = response.data.total
+
+        // Just to simulate the time of the request
+        setTimeout(() => {
+          this.listLoading = false
+        }, 1.5 * 1000)
+      })
+    },
+    handleFilter() {
+      this.listQuery.pageNum = 1
+      this.getList()
+    },
+    handleCopy(text, event) {
+      clip(text, event)
+    },
+    handleRowClick({ id }) {
+      //   this.$router.push(`/approval/token/detail/${id}`)
+    },
+    sortChange(data) {
+      const { prop, order } = data
+      if (prop === 'id') {
+        this.sortByID(order)
+      }
+    },
+    sortByID(order) {
+      if (order === 'ascending') {
+        this.listQuery.sort = '+id'
+      } else {
+        this.listQuery.sort = '-id'
+      }
+      this.handleFilter()
+    },
+    handleUser(type, row) {
+      const initData = JSON.parse(JSON.stringify(initUserData))
+      this.dialogFormVisible = true
+      this.dialogType = type
+      this.userData = type == 'edit' ? Object.assign(initData, row) : initData
+     
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+        this.$refs.tree.setCheckedKeys(this.userData.permission)
+        window.$refs =this
+      })
+    },
+    async handleSave() {
+      
+      this.$refs['dataForm'].validate(valid => {
+     
+        if (!valid) return
+        this.submitLoading = true
+        const { loginName, password, id } = this.userData
+        const permission = this.$refs.tree.getCheckedKeys()
+        const requetst = this.dialogType == 'edit' ? update({ id, loginName, password, permission }) : update({ loginName, password, permission })
+        requetst
+          .then(res => {
+            this.getList(true)
+
+            this.$notify({
+              title: '提示',
+              message: '提交成功',
+              type: 'success',
+              duration: 2000
+            })
+            this.submitLoading = false
+            this.dialogFormVisible = false
+          })
+          .catch(err => {
+            this.submitLoading = false
+            this.getList(true)
+          })
+      })
+    },
+
+    getSortClass: function(key) {
+      const sort = this.listQuery.sort
+      return sort === `+${key}` ? 'ascending' : 'descending'
+    }
+  }
+}
+</script>
